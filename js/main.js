90153897f8952d289b99d94d6b78fa3c4b4e5c5e
Simone Viglione <zimoviglio51@gmail.com>
2023-02-28 16:27:09 +0100

    implementato next

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
 const images = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp" ];
 
+const slider = document.querySelector(".slider");
+
+for (let i = 0; i < images.length; i++) {
+
+    const imageContainer =  
+
+    `<div class="image_container">
+        <img class=image src="${images[i]}"/>
+    </div>`;
+
+    slider.innerHTML += imageContainer;
+}
+
+const   imageContainerArray = document.getElementsByClassName("image_container");
+
+let currentImage = 0;
+
+imageContainerArray[currentImage].classList.add("d-block");
+
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+next.addEventListener("click", function() {
+
+    imageContainerArray[currentImage].classList.remove("d-block");
+    currentImage++;
+    imageContainerArray[currentImage].classList.add("d-block");
+    }
+);
+
